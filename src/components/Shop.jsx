6d65782e4ad2c12da82412286b077e30ea9aa2a8
varekai-dev <serhijsav@gmail.com>
2021-01-20 22:23:37 +0200

    cart

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,55 +1,59 @@
-import React, { useState, useEffect } from 'react'
-import { API_KEY, API_URL } from '../config'
-import Preloader from './Preloader'
-import GoodsList from './GoodsList'
-import Cart from './Cart'
+import React, { useState, useEffect } from "react";
+import { API_KEY, API_URL } from "../config";
+import Preloader from "./Preloader";
+import GoodsList from "./GoodsList";
+import Cart from "./Cart";
 
 function Shop() {
-	const [goods, setGoods] = useState([])
-	const [loading, setLoading] = useState(true)
-	const [order, setOrder] = useState([])
+  const [goods, setGoods] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [order, setOrder] = useState([]);
 
-	const addToBasket = (item) => {
-		const itemIndex = order.findIndex((orderItem) => orderItem.id === item.id)
-		if (itemIndex < 0) {
-			const newItem = {
-				...item,
-				quantity: 1
-			}
-			setOrder([newItem])
-		} else {
-			const newOrder = order.map((orderItem, index) => {
-				if (index === itemIndex) {
-					return {
-						...orderItem,
-						quantity: orderItem.quantity + 1
-					}
-				} else {
-					return orderItem
-				}
-			})
-			setOrder(newOrder)
-		}
-	}
-	console.log(order)
-	useEffect(function getGoods() {
-		fetch(API_URL, {
-			headers: {
-				Authorization: API_KEY
-			}
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				data.featured && setGoods(data.featured)
-				setLoading(false)
-			})
-	}, [])
-	return (
-		<main className="container content">
-			<Cart quantity={order.length} />
-			{loading ? <Preloader /> : <GoodsList goods={goods} addToBasket={addToBasket} />}
-		</main>
-	)
+  const addToBasket = (item) => {
+    const itemIndex = order.findIndex((orderItem) => orderItem.id === item.id);
+    if (itemIndex < 0) {
+      const newItem = {
+        ...item,
+        quantity: 1,
+      };
+      setOrder([...order, newItem]);
+    } else {
+      const newOrder = order.map((orderItem, index) => {
+        if (index === itemIndex) {
+          return {
+            ...orderItem,
+            quantity: orderItem.quantity + 1,
+          };
+        } else {
+          return orderItem;
+        }
+      });
+      setOrder(newOrder);
+    }
+  };
+  console.log(order);
+  useEffect(function getGoods() {
+    fetch(API_URL, {
+      headers: {
+        Authorization: API_KEY,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        data.featured && setGoods(data.featured);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <main className="container content">
+      <Cart quantity={order.length} />
+      {loading ? (
+        <Preloader />
+      ) : (
+        <GoodsList goods={goods} addToBasket={addToBasket} />
+      )}
+    </main>
+  );
 }
 
-export default Shop
+export default Shop;
