92b6e73e2d65ed3dfc277c1ae282cdd4d0502472
varekaiwork <serhiisaviukwork@gmail.com>
2021-01-21 15:41:44 +0200

    finish

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
 			<div className="footer-copyright">
 				<div className="container">
 					© {new Date().getFullYear()} Copyright Text
-					<a className="grey-text text-lighten-4 right" href="#!">
+					<a className="grey-text text-lighten-4 right" href="https://github.com/varekai-dev/react-shop">
 						Repo
 					</a>
 				</div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ function Header() {
 	return (
 		<nav className=" cyan darken-2">
 			<div className="nav-wrapper">
-				<a href="!#" className="brand-logo">
+				<a href="/" className="brand-logo">
 					React Shop
 				</a>
 				<ul id="nav-mobile" className="right hide-on-med-and-down">
 					<li>
-						<a href="!#">Repo</a>
+						<a href="https://github.com/varekai-dev/react-shop">Repo</a>
 					</li>
 				</ul>
 			</div>
